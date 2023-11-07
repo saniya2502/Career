cb578ddfd619efa43abf56fdf501d41d833f89ba
Saniya Inamdar <saniainamdar2522@gmail.com>
2023-11-08 00:05:13 +0530

    changes in contact,logout and resume

--- resume.review.php
+++ resume.review.php
@@ -1,79 +1,77 @@
-<?php 
-	ob_start();
-	session_start(); 
+<?php
+ob_start();
+session_start();
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<link rel="stylesheet" href="css/search.view.css" type="text/css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="css/search.view.css" type="text/css" />
 </head>
 
-<?php 
-	$conn = mysql_connect('localhost', 'root', '');
-	$db = mysql_select_db('jobsportal', $conn);
-	if($conn == false){ echo mysql_errno($conn).":".mysql_error($conn)."\n"; }
-	if($db == false){ echo mysql_errno($conn).":".mysql_error($conn)."\n"; }
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'jobsportal');
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
 ?>
 
 <body>
-	<div style="margin-left:45%">
-		<b><font color="#FFFFFF">Upload New Resume</font></b>
-	</div>
-	<div id="body_struct" style="margin-top: 1.5%">
-<?php
-	$id = $_SESSION['SEEK_ID'];
-	$query = "SELECT resume FROM seeker_details WHERE id='$id'";
-	$query_run = mysql_query($query);
-	
-	$file = mysql_result($query_run, 0, 'resume');
-	$handle = fopen($file, 'r');
-	$read = file($file);
-	foreach($read as $fname)
-	{
-		echo $fname.'<br />';
-	}
-	fclose($handle);
-?>
-	</div id="body_struct">
+<div style="margin-left: 45%">
+    <b><font color="#FFFFFF">Upload New Resume</font></b>
+</div>
+<div id="body_struct" style="margin-top: 1.5%">
+    <?php
+    $id = $_SESSION['SEEK_ID'];
+    $query = "SELECT resume FROM seeker_details WHERE id = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($resume);
 
-	<div style="margin-left: 9.7%">
-		<form action="resume.review.php" method="post" enctype="multipart/form-data">
-			<input type="file" name="new_resume" value="file" />
-			<input type="submit" name="upload_new_resume" value="Save" / style="margin-left: 65%; -webkit-border-radius: 8px; font-size: 15px;">
-		</form>
-	</div>
+    while ($stmt->fetch()) {
+        $handle = fopen($resume, 'r');
+        while (($line = fgets($handle)) !== false) {
+            echo $line . '<br />';
+        }
+        fclose($handle);
+    }
+    ?>
+</div>
+
+<div style="margin-left: 9.7%">
+    <form action="resume.review.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="new_resume" />
+        <input type="submit" name="upload_new_resume" value="Save" style="margin-left: 65%; -webkit-border-radius: 8px; font-size: 15px;">
+    </form>
+</div>
 </body>
 
-<?php 
-	if(isset($_POST['upload_new_resume']))
-	{
-		if(isset($_FILES['new_resume']))
-		{
-			if(!empty($_FILES['new_resume']))
-			{
-				$filename = $_FILES['new_resume']['name'];
-				$new_filename = $db_id.'_'.$filename;
-				$folder = 'upload/seeker_resume/';
-				$format = substr($filename, strpos($filename, '.') + 1);
-					
-				$upload_cv = $folder.$db_id.'_'.$filename;
-					
-				if($format == doc || $format == docx)
-				{
-					move_uploaded_file($_FILES['new_resume']['tmp_name'], $folder.$new_filename);
-					$query = "UPDATE seeker_details SET resume='$upload_cv' WHERE id='$id'";
-					$query_run = mysql_query($query);
-				}
-				
-				else
-				{ 
-					echo '<script> alert("File not supported. Select only document file ie .doc or .docx"); </script>';
-				}	
-			}
-		}
-	}
+<?php
+if (isset($_POST['upload_new_resume'])) {
+    if (isset($_FILES['new_resume'])) {
+        $id = $_SESSION['SEEK_ID'];
+        $upload_dir = 'upload/seeker_resume/';
+        $format = pathinfo($_FILES['new_resume']['name'], PATHINFO_EXTENSION);
+        $new_filename = $id . '_' . $_FILES['new_resume']['name'];
+        $upload_cv = $upload_dir . $new_filename;
+
+        if ($format === 'doc' || $format === 'docx') {
+            move_uploaded_file($_FILES['new_resume']['tmp_name'], $upload_cv);
+            $query = "UPDATE seeker_details SET resume = ? WHERE id = ?";
+            $stmt = $mysqli->prepare($query);
+            $stmt->bind_param("si", $upload_cv, $id);
+            if ($stmt->execute()) {
+                echo '<script>alert("Resume uploaded successfully");</script>';
+            } else {
+                echo '<script>alert("Failed to upload resume");</script>';
+            }
+        } else {
+            echo '<script>alert("File not supported. Select only document file i.e., .doc or .docx");</script>';
+        }
+    }
+}
 ?>
 </html>
